4302119c59a47a955d86061128fa1c330736954f
Divy Patel <67093305+divypatel56@users.noreply.github.com>
2024-05-02 15:48:31 +0000

    Testing PHP variables

--- index.php
+++ index.php
@@ -8,9 +8,12 @@
     $name = "PHP";
     $car = "Toyota";
     $color = "red";    
+    $x = 5;
+    $y = 7;
     echo "I have started learning $name.";
     ECHO "<br> PHP is a server-side scripting language.<br>";
     echo "I have a $color $car.";
+    echo "<br>x+y = ", $x+$y;
     
     ?> 
     
