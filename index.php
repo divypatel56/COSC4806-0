028ac6120e49bc7674658b6ccee618210ea24900
Divy Patel <67093305+divypatel56@users.noreply.github.com>
2024-05-07 17:59:46 +0000

    If else

--- index.php
+++ index.php
@@ -7,36 +7,22 @@
     <?php  
     $name = "PHP";
     $car = "Toyota";
-    $color = "red";    
-    $x = 5;
-    $y = 7;
-    echo "I have started learning $name.";
-    ECHO "<br> PHP is a server-side scripting language.<br>";
-    echo "I have a $color $car.";
-  
+    $color = "red"; 
+    $cost = 0;
 
-    function add(){
-      $z;
-      global $x, $y;
-      $z = $x + $y;
-      echo "<br>The sum of $x and $y is $z."."<br>";
+    if($color=="red"){
+      echo "The color is red<br>";
     }
-    add();
-    print "We can also use print instead of echo.<br>";
-    //String functions
-    echo strlen("Hello World")."<br>";
-    echo str_word_count("Hello My name is Divy")."<br>";
-    echo strpos("Hello world!", "world")."<br>";
-    echo strtoupper("divy")."<br>";
-    echo strtolower("DIVY")."<br>";
-    echo str_replace("World", "Class" , "Hello World!")."<br>";
-    echo strrev("World")."<Br>";
+    else{
+      echo "The color is not red<br>";
+    }
 
-    //String concate
-    $txt1 = "COSC";
-    $txt2 = "4806";
-    echo($txt1 ."-". $txt2);
-   
+    if ($cost == 0) {
+      Echo "no cost";
+    } else {
+      Echo "cost is " . $cost;;
+    }
+
     
     ?> 
     
