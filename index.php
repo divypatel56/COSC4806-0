a43910c45b811858d0c88f0f43278e86737a3a68
Divy Patel <67093305+divypatel56@users.noreply.github.com>
2024-05-07 22:16:47 +0000

    Associative Arrays

--- index.php
+++ index.php
@@ -5,33 +5,26 @@
   <body>
     <h2>My First PHP Page!</h2> 
     <?php  
-    echo("<pre>");
-    $products = array("iPhone", "iPad", "MacBook","Book");
+    //Associative array
+    $employee = array("Name"=>'John',"Id"=>"1", "Salary"=>"2000");
+    echo "<pre>";
+    foreach($employee as $x=>$y){
+      echo "$x:$y<br>";
+    };
 
-    $prices = array(1000, 2000, 3000, 4000);
+    print_r(array_keys($employee));
+    print_r(array_values($employee));
 
-    foreach($products as $p){
-      echo $p . "<br>";
+    //ArrayKey
+    if(array_key_exists("Name",$employee)){
+      echo "$employee[Name] is present employee";
+    }
+    else{
+      echo "Name is not present";
     }
 
-    echo "The price of the ". $products[0]." is ". $prices[0].".<br>";
-
-    print_r($products);
-    echo("<pre>");
-    print_r($prices);
-    //To remove element from array
-    unset($prices[2]);
-    echo "<pre>";
-    print_r($prices);
-
-    // Combinations of arrays
-    $odd = [1,3,5,7,9];
-    $even = [2,4,6,8,10];
-    $numbers = array_merge($odd, $even);
-    echo "<pre>";
-    print_r($numbers);
-
-    echo count($numbers);
+    
+    
     
     ?> 
     
