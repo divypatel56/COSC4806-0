4f9beff96b72dc3fbd77964523088283dfbbd213
Divy Patel <67093305+divypatel56@users.noreply.github.com>
2024-05-07 23:06:26 +0000

    Exercise 1 Create movie & contact Table

--- index.php
+++ index.php
@@ -3,26 +3,45 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <h2>My First PHP Page!</h2> 
     <?php  
-    //Associative array
-    $employee = array("Name"=>'John',"Id"=>"1", "Salary"=>"2000");
-    echo "<pre>";
-    foreach($employee as $x=>$y){
-      echo "$x:$y<br>";
-    };
+    //Define arrays
+    $movies = array(
+      array("Inception",  "Sci-Fi"),
+      array( "The Dark Knight","Action"),
+      array( "The Matrix", "Sci-Fi")
+    );
 
-    print_r(array_keys($employee));
-    print_r(array_values($employee));
-
-    //ArrayKey
-    if(array_key_exists("Name",$employee)){
-      echo "$employee[Name] is present employee";
+    
+    // Generate HTML for Movie table
+    echo "<h2>Movie Table</h2>" ;
+    echo "<table border='1'>";
+    echo "<tr><th>Movie Name</th><th>Genre</th></tr>";
+    foreach( $movies as $movie){
+      echo "</tr>";
+      foreach($movie as $value){
+        echo "<td>".$value."</td>";
+      }
+      echo "</tr>";
     }
-    else{
-      echo "Name is not present";
-    }
+    echo"</table>";
+    //Conta
+    $contact = array(
+      array("John","234675231","johan@gmail.com"),
+      array("Jane","217864528","jane@gmail.com"),
+      array("Jack","234567890","jack@gmail.com")
+    );
 
+    echo "<h2>Contact Table</h2>" ;
+    echo "<table border = 1";
+    echo "<tr> <th>Name</th> <th>Phone</th> <th>Email</th> </tr>";
+    foreach($contact as $key){
+      echo "</tr>";
+      foreach($key as $value){
+        echo "<td>" .$value . "</td>";
+      }
+      echo "</tr>";
+    }
+    echo "</table>";
     
     
     
